98bd21e7a09d44bb707f77bd8f58bd17d6ce11bd
zhekamegarep <zhekamegarep@mail.ru>
2020-07-19 23:30:44 +0300

    Initial commit

---
+++ Main/templates/includes/header.php
@@ -0,0 +1,7 @@
+<header class="mainheader">
+    <ul class="headermenu">
+        <li id="header_mainbtn"><a href = "http://127.0.0.1:8000">Главная Страница</a></li>
+        <li id="header_nothingbtn"><a href = "#">Кнопка, которая ничего не делает</a></li>
+        <li id="header_secondbtn"><a href = "#">Вторая кнопка ._.</a></li>
+    </ul>
+</header>
---
+++ Main/templates/index.php
@@ -0,0 +1,12 @@
+{% extends "wrapper.html" %}
+
+{% block title %}
+Главная страница
+{% endblock %}
+
+{% block content %}
+<p>Hello, motherfucker</p>
+
+{% include "includes/testinclude.html" %}
+
+{% endblock %}
