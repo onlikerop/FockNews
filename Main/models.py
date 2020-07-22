221c8e8eb339179fa6f897f8343e2be2f502857d
zhekamegarep <zhekamegarep@mail.ru>
2020-07-22 21:29:40 +0300

    Added news on general page
    The general page was updated, and now there are displaying last 20 news (articles). Also added link on admin panel instead of useless button, refactored some code

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField(max_length=120)
+    body = models.TextField()
+    pub_datetime = models.DateTimeField()
+    lasted_datetime = models.DateTimeField()
+    author = models.CharField(max_length=24)
+    tags = models.TextField()
+    status = models.CharField(max_length=24)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
--- Main/urls.py
+++ Main/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.views.generic import ListView, DetailView
+from Main.models import Articles
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path(
         '',
-        views.index,
-        name='index'
+        ListView.as_view(queryset=Articles.objects.all().order_by("-pub_datetime")[:20],
+                         template_name="index.html")
     ),
     path(
         'contacts/',
