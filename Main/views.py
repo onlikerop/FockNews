d5c087425a28587461da60e526e33239b088b88c
zhekamegarep <zhekamegarep@mail.ru>
2020-07-22 21:30:06 +0300

    Added custom error 404 page

--- Main/views.py
+++ Main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 
 
 def index(request):
     return render(request, 'index.html')
+
+
 def contacts(request):
     return render(request, 'contacts.html')
