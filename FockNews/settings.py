bd37420dbcf686216d304d7f3e2ac33812f4c4f3
zhekamegarep <zhekamegarep@mail.ru>
2020-07-26 15:27:12 +0300

    Small settings changes

--- FockNews/settings.py
+++ FockNews/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'FockNews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+3'
 
 USE_I18N = True
 
