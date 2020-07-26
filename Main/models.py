c14ed83add413ef6ff4bcbd1ff44c40ced16a2c9
zhekamegarep <zhekamegarep@mail.ru>
2020-07-26 19:13:39 +0300

    Some changes in db

--- Main/models.py
+++ Main/models.py
@@ -5,8 +5,13 @@ class Articles(models.Model):
     title = models.CharField(max_length=120)
     body = models.TextField()
     create_datetime = models.DateTimeField()
-    pub_datetime = models.DateTimeField(blank=True, null=True)
-    lasted_datetime = models.DateTimeField(blank=True, null=True)
+    pub_datetime = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+    lasted_datetime = models.DateTimeField(
+        blank=True,
+        null=True)
     author = models.CharField(max_length=24)
     tags = models.TextField()
     status = models.CharField(max_length=24)
