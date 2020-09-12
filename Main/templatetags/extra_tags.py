66e7720b18ea2e4277655fb4a904bcbf3b7991db
zhekamegarep <zhekamegarep@mail.ru>
2020-09-12 17:14:01 +0300

    Remigrated DB, released bans
    Dropped DB, remigrated and little changed. Also created check_ban function and displaying ban's information on user page (i exists).

--- Main/templatetags/extra_tags.py
+++ Main/templatetags/extra_tags.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+
 from django import template
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+from Main.models import Articles
+from Users.models import Profile, Bans
 
 register = template.Library()
 
@@ -6,3 +13,23 @@ register = template.Library()
 @register.simple_tag()
 def check_perm(user, perm):
     return user.has_perm(perm)
+
+
+@register.simple_tag()
+def check_ban(user):
+    is_banned = bool(Bans.objects.filter(Q(user=user)
+                                         & ~Q(status='canceled')
+                                         & ~Q(status='paused')
+                                         & Q(pass_datetime__gt=datetime.now().strftime("%Y-%m-%d %H:%M:%d"))
+                                         ).order_by("-ban_datetime").count())
+    result = {
+        'is_banned': is_banned
+    }
+    if is_banned:
+        result = dict(result, data=Bans.objects.filter(Q(user=user)
+                                                       & ~Q(status='canceled')
+                                                       & ~Q(status='paused')
+                                                       & Q(
+            pass_datetime__gt=datetime.now().strftime("%Y-%m-%d %H:%M:%d"))
+                                                       ).order_by("-ban_datetime")[0])
+    return result
