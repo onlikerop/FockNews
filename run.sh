3867bc5f0446aec5bc5f2260e3795d0f19f8fcc5
Onlikerop <zhekamegarep@mail.ru>
2021-12-31 15:17:08 +0300

    Changed slashes

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 docker run \
 -p 8000:8000 \
--v \\var\\www\\FockNews\\volume:\\usr\\src\\FockNews\\volume \
+-v /var/www/FockNews/volume:/usr/src/FockNews/volume \
 -it --rm \
 --name focknews onlikerop/focknews
